178aa2ed83ff56f9458d253b4aaf151c30c56380
ZaidYounan <zaid.younan@gmail.com>
2021-08-16 13:35:45 +1000

    Add useState to Form, create handleChange function to pass input updates to browser

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App(props) {
 
   return (
     <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
+      <h1>Do-Too</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         <Filter/>
