afba827029c5d526faac415c9ee7793574b85bae
ZaidYounan <zaid.younan@gmail.com>
2021-08-17 18:30:18 +1000

    Remove placeholder tasks, create edit function and seperate Todo component into Viewing and Editing mode

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App(props) {
     completed={task.completed} 
     toggleTaskCompleted={toggleTaskCompleted}
     deleteTask={deleteTask}
+    editTask={editTask}
     />
   ));
 
@@ -28,6 +29,21 @@ function App(props) {
     setTasks([...tasks, newTask]);
   }
 
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+      if (id === task.id) {
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map(task => {
       if (id === task.id) {
@@ -36,11 +52,6 @@ function App(props) {
       return task;
     });
     setTasks(updatedTasks);
-  }
-
-  function deleteTask(id) {
-    const remainingTasks = tasks.filter(task => id !== task.id);
-    setTasks(remainingTasks);
   }
 
   return (
